c7af1b4320bea4db24711fa83b97561b4516f5c1
Jan-Otto Kröpke <joe@adorsys.de>
2022-02-11 09:17:30 +0100

    Fix nodejs builds

--- ubi-8/12/root/docker-entrypoint.sh
+++ ubi-8/12/root/docker-entrypoint.sh
@@ -2,17 +2,25 @@
 
 set -euo pipefail
 
-if [ -f /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
-  if [ "$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)" != "9223372036854771712" ]; then
-    if [ "${NODE_MEMORY_LIMIT_PERCENT}" -lt 1 ] || [ "${NODE_MEMORY_LIMIT_PERCENT}" -gt 100 ]; then
-      echo "\$NODE_MEMORY_LIMIT_PERCENT should be between 1 and 99."
-      exit 1
-    fi
-
-    NODE_OPTIONS="--max_old_space_size=$(( $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) * NODE_MEMORY_LIMIT_PERCENT / 100 / 1000 / 1000 )) ${NODE_OPTIONS:-}"
-    export NODE_OPTIONS
-  fi
+if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+  MEMORY=$(cat /sys/fs/cgroup/memory.max)
+elif [ -f /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
+  MEMORY=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+else
+  MEMORY=9223372036854771712
 fi
+
+if [ "${MEMORY}" != "max" ] && [ "${MEMORY}" != "9223372036854771712" ]; then
+  if [ "${NODE_MEMORY_LIMIT_PERCENT}" -lt 1 ] || [ "${NODE_MEMORY_LIMIT_PERCENT}" -gt 100 ]; then
+    echo "\$NODE_MEMORY_LIMIT_PERCENT should be between 1 and 99."
+    exit 1
+  fi
+
+  NODE_OPTIONS="--max_old_space_size=$(( MEMORY * NODE_MEMORY_LIMIT_PERCENT / 100 / 1000 / 1000 )) ${NODE_OPTIONS:-}"
+  export NODE_OPTIONS
+fi
+
+unset MEMORY
 
 # From https://docs.openshift.com/container-platform/3.9/creating_images/guidelines.html#use-uid
 if ! whoami > /dev/null 2>&1; then
